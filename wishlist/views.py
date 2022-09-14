892f836c523d8bab3d6e111eb00c3ea748805d2d
Bellalpsb <salsabiella4@gmail.com>
2022-09-14 22:31:27 +0700

    update function in views.py

--- wishlist/views.py
+++ wishlist/views.py
@@ -25,7 +25,9 @@ def show_json_by_id(request, id):
     data = BarangWishlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
-
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 
 
