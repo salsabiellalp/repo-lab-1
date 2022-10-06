e8827665db85c1adaeb71d82b6cbe48a15ac0f5e
Bellalpsb <salsabiella4@gmail.com>
2022-10-06 14:37:01 +0700

    lab 5

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from wishlist.views import show_wishlist
+from wishlist.views import show_wishlist, show_ajax
 from wishlist.views import show_xml 
 from wishlist.views import show_json
 from wishlist.views import show_json_by_id 
@@ -20,4 +20,5 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('ajax/', show_ajax, name='show_ajax'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -23,6 +23,17 @@ def show_wishlist(request):
     }
     return render(request, "wishlist.html", context)
 
+@login_required(login_url='/wishlist/ajax/')
+def show_ajax(request):
+    # data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        # 'list_barang': data_barang_wishlist,
+        'nama': 'Bella',
+        'last_login': request.COOKIES.get('last_login'),
+    }
+    return render(request, "wishlist_ajax.html", context)
+
+
 def show_xml(request):
     data = BarangWishlist.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
